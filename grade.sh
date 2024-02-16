875a6418a2f6c47dc4fbbee8f988671086c494a9
HGregersen <hgregersen@ucsd.edu>
2024-02-15 19:17:33 -0800

    week 6

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,40 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[ -f student-submission/ListExamples.java ]]
+then
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+
+else
+    echo "Missing student-submission/ListExamples.java, did you forget the file or misname it?"
+    exit 1 #nonzero exit code to follow convention
+fi
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+  echo "The program failed to compile, see compile error above"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+echo $lastline
+firstword=$(echo $lastline | awk -F'[(]' '{print $1}')
+echo $firstword
+if [[ $firstword -eq 'OK' ]]
+then
+    echo "YIPPPEEEEEEEEEEEEE YOU PASSED"
+fi
+
+tests=$(echo $lastline | awk -F'[(], [, ]' '{print $3}')
+echo $tests
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=($tests - $failures)
+
+echo "Your score is $successes / $tests"
